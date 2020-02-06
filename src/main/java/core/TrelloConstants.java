3d97948a14443bcde26cc38c7622aba7e5e3e54c
Olga Ivanova <olga_ivanova@epam.com>
2020-02-07 01:21:14 +0300

    update key and token

--- src/main/java/core/TrelloConstants.java
+++ src/main/java/core/TrelloConstants.java
@@ -1,8 +1,8 @@
 package core;
 
 public class TrelloConstants {
-    public static final String TRELLO_API_KEY_VALUE = "cfec53e08b8393f77dd65ba6525039c8";
-    public static final String TRELLO_API_TOKEN_VALUE = "2f58ade2a9219cc72027f740317288f8afbf0045a49cbbacfdcf2c9fe3612610";
+    public static final String TRELLO_API_KEY_VALUE = "YOUR_KEY";
+    public static final String TRELLO_API_TOKEN_VALUE = "YOUR_TOKEN";
 
     public static final String TRELLO_ALL_BOARDS_API_URL = "https://api.trello.com/1/members/me/boards?fields=name,url";
     public static final String TRELLO_NEW_BOARD_API_URL = "https://api.trello.com/1/boards/";
